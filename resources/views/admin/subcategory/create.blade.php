6d621fa24b500311c8808f4ed994688950a3efb9
ramananda-at-251313237507 <rshc4u@gmail.com>
2024-02-16 14:24:58 +0600

    added sub-category form

---
+++ resources/views/admin/subcategory/create.blade.php
@@ -0,0 +1,76 @@
+@extends('admin.layouts.main')
+
+@section('content')
+  <div class="container-fluid" id="container-wrapper">
+      <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Sub Category</h1>
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="./">Home</a></li>
+              <li class="breadcrumb-item active" aria-current="page">Category</li>
+            </ol>
+          </div>
+
+          <div class="row justify-content-center">
+            @if(Session::has('message'))
+                <div class="alert alert-success">{{Session::get('message')}}</div>
+            @endif
+            <div class="col-lg-10">
+              <!-- Form Basic -->
+              <form action="{{route('subcategory.store')}}" method="POST">@csrf
+              
+              <div class="card mb-4">
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                  <h6 class="m-0 font-weight-bold text-primary">Create Sub Category</h6>
+                </div>
+                <div class="card-body">
+                  <form>
+                    <div class="form-group">
+                      <label for="name">Name</label>
+                      <input  id="name" name = "name"
+                        placeholder="Enter name of category" class="form-control @error('name') is-invalid @enderror">
+
+                          @error('name')
+                                  <span class="invalid-feedback" role="alert">
+                                      <strong>{{ $message }}</strong>
+                                  </span>
+                          @enderror
+                    
+                    </div>
+                    
+                    
+                    <div class="form-group">
+                      <div class="custom-file">
+                        <label>Choose Category</label>
+                        <select class="form-control @error('category') is-invalid @enderror" name="category">
+                            <option value="">Select Category</option>
+                            @foreach(\App\Models\Category::all() as $category)
+                            <option value="{{$category->id}}">{{$category->name}}</option>
+                            @endforeach
+                        </select>
+                    
+                        @error('category')
+                                  <span class="invalid-feedback" role="alert">
+                                      <strong>{{ $message }}</strong>
+                                  </span>
+                          @enderror
+                      </div>
+                      
+                    </div>
+
+                    <div class="form-group" >
+                      <button  class="btn btn-primary">Submit</button>
+
+                    </div>
+                    
+                  </form>
+                </div>
+              </div>
+
+              </form>
+            </div>
+            
+          </div>
+   
+</div> 
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::resource('category', 'CategoryController');
 
+Route::resource('subcategory', 'SubcategoryController');
 
 Route::resource('test', 'TestController');
 
