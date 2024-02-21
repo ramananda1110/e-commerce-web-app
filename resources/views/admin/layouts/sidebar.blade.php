7943cfadb55500e704470450651cc3da6ddfad86
ramananda-at-251313237507 <rshc4u@gmail.com>
2024-02-21 14:38:13 +0600

    add menu slide in product items

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -48,6 +48,22 @@
       </li>
 
       <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseBootstrapProduct"
+          aria-expanded="true" aria-controls="collapseBootstrapProduct">
+          <i class="far fa-fw fa-window-maximize"></i>
+          <span>Product</span>
+        </a>
+        <div id="collapseBootstrapProduct" class="collapse" aria-labelledby="headingBootstrap" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Product</h6>
+            <a class="collapse-item" href="{{route('product.index')}}">View</a>
+            <a class="collapse-item" href="{{route('product.create')}}">Create</a>
+         
+          </div>
+        </div>
+      </li>
+
+      <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseForm" aria-expanded="true"
           aria-controls="collapseForm">
           <i class="fab fa-fw fa-wpforms"></i>
